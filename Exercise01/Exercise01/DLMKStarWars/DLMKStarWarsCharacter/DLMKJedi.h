c32d23cc5cc86b76b660ee59766685f1ced3356c
Dolmake <demondolmake@hotmail.com>
2015-01-07 09:47:47 +0100

    Creating Jedi Class

---
+++ Exercise01/Exercise01/DLMKStarWars/DLMKStarWarsCharacter/DLMKJedi.h
@@ -0,0 +1,14 @@
+//
+//  DLMKJedi.h
+//  Exercise01
+//
+//  Created by Daniel on 07/01/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DLMKStarWarsCharacter.h"
+
+@interface DLMKJedi : NSObject
+
+@end
---
+++ Exercise01/Exercise01/DLMKStarWars/DLMKStarWarsCharacter/DLMKJedi.m
@@ -0,0 +1,13 @@
+//
+//  DLMKJedi.m
+//  Exercise01
+//
+//  Created by Daniel on 07/01/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import "DLMKJedi.h"
+
+@implementation DLMKJedi
+
+@end
