03fe61a45a0d1e2e0bcb5d38e51fc848667ef6dd
Dolmake <demondolmake@hotmail.com>
2015-01-07 09:28:46 +0100

    Xcode empty project

---
+++ Exercise01/Exercise01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exercise01
+//
+//  Created by Daniel on 07/01/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
